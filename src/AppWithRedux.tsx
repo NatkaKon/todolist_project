87226256fe45cdfe9eebb21fd4097eb87f3ac5eb
Natalia Kondratieva <kondratieva07@gmail.com>
2023-01-28 20:32:17 +0300

    update route

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -63,8 +63,8 @@ function AppWithRedux() {
                     <Route path="/" element={<TodolistsList/>}/>
                     <Route path="/login" element={<Login/>}/>
 
-                    <Route path="404" element={<h1 style={{textAlign: 'center'}}>404: PAGE NOT FOUND</h1>}/>
-                    <Route path="*" element={<Navigate to="404"/>}/>
+                    <Route path="/404" element={<h1 style={{textAlign: 'center'}}>404: PAGE NOT FOUND</h1>}/>
+                    <Route path="*" element={<Navigate to="/404"/>}/>
                 </Routes>
             </Container>
         </div>
